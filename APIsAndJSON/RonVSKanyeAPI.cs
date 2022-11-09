b7dd22bddb900556b085be5cf0df452bbd94e1c5
gamejunkiez <queenangrylobster@gmail.com>
2022-11-09 14:08:07 -0600

    completed

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static System.Random;
 namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
+        public static void PrintKanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = "Kanye West: " + JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine(kanyeQuote);
+        }
+
+        public static void PrintRonQuote()
+        {
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = "Ron Swanson: " + JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            Console.WriteLine(ronQuote);
+        }
+        // Slows down the program at random intervals and prints "..."
+        public static void AwkwardSilence()
+        {
+            Random rnd = new Random();
+            var lengthOfSilence = (rnd.Next(1, 5) * 1000);
+            Console.WriteLine("...");
+            Thread.Sleep(lengthOfSilence);
+        }
     }
 }
